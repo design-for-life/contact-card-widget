7ec0f859b5d63b116edee8fbfffdef5690b51cfd
Sachin Lala <saroohanikunj@Family-laptop.local>
2025-01-02 02:41:26 +0530

    created basic form and contact-card

---
+++ src/test/sample-profile.js
@@ -0,0 +1,7 @@
+const profileData = {
+  name: "Sachin Lala",
+  location: "Bengaluru, Karnataka, India",
+  linkedin: "https://www.linkedin.com/in/sachinlala/",
+  email: "sachin.lala@gmail.com",
+  phone: "+91-9886119507"
+};
